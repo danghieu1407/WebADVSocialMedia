1698a555ef5cf83de719a84371a8e88a0a9f7429
unknown <cbt270801@gmail.com>
2021-12-17 13:19:35 +0700

    Chia ejs Sau nay co them du lieu phai thao tac trong App.js

--- key.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    googleClientID: '  609305227636-r59vkh4v6p5t2cf99gf36pbp2tsvrg4h.apps.googleusercontent.com',
-    googleClientSecret: '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'
-  };
-
