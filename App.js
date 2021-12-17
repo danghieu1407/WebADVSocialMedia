d8a0fc44ecbe1b19dcb2dcb86b7e56f1fdb4d5e4
unknown <cbt270801@gmail.com>
2021-12-17 21:01:16 +0700

    layout ejs

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 var express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 var app = express();
 
+app.use(express.static("public"));
 // set the view engine to ejs
+app.use(expressLayouts);
+app.set("layout","./Pages/layout");
 app.set('view engine', 'ejs');
 
 // use res.render to load up an ejs view file
@@ -11,9 +15,15 @@ app.get('/', function(req, res) {
   res.render('Pages/index');
 });
 
+
+app.get("/about", function(req, res) {
+  res.render("Pages/about");
+})
+
 // about page
 app.get('/about', function(req, res) {
   res.render('Pages/about');
 });
 
 app.listen(3000);
+console.log("Server started on port 3000");
