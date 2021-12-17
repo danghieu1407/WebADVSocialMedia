5122fd5cec1a18d2cdc305d042325460015139b4
unknown <cbt270801@gmail.com>
2021-12-17 22:50:00 +0700

    Chia file lan 2

--- App.js
+++ App.js
@@ -7,15 +7,23 @@ const db = require('./db')
 app.use(express.static("public"));
 // set the view engine to ejs
 app.use(expressLayouts);
-app.set("layout","./Pages/layout");
+app.set("layout","./Layout/layout");
 app.set('view engine', 'ejs');
+app.set("layout extractScripts", true)
 
 // use res.render to load up an ejs view file
 
 // index page
+
 app.get('/', function(req, res) {
   res.render('Pages/index');
 });
+
+
+app.get('/login', (req, res) => {
+  console.log(req.params.page);
+  res.render('Layout/login', {layout: `./Layout/login` })
+  })
 
 
 app.get("/about", function(req, res) {
