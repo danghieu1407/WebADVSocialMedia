f0b6bc79e49c891433cb9b6347b28ab12a8bf5a3
danghieu1407 <danghieu140701@gmail.com>
2021-12-20 11:31:05 +0700

    [post]

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 var express           = require('express');
+var bodyParser        = require('body-parser')
 var app               = express();
 var authRouter        = require('./Routes/auth');
 var UserRouter        = require('./Routes/user');
 var IndexRouter       = require('./Routes/index');
+
+
 
 const expressLayouts  = require('express-ejs-layouts');
 app.set("layout","./Layout/layout");
@@ -12,10 +15,14 @@ app.set("layout extractScripts", true)
 
 
 app.use(express.static("public"));
+app.use(bodyParser.json())
+
 app.use(expressLayouts);
 app.use("",IndexRouter);
 app.use('/auth', authRouter);
 app.use("/user",UserRouter);
+
+
 
 const port = process.env.PORT || 8080;
 app.listen(port);
--- Routes/index.js
+++ Routes/index.js
@@ -1,4 +1,6 @@
+const { response } = require('express');
 const express = require('express');
+const bodyParser = require('body-parser')
 const router = express.Router();
 var session = require('express-session');
 var passport = require('passport');
@@ -10,12 +12,16 @@ router.use(session({
 router.use(passport.initialize());
 router.use(passport.session());
 
-
-
+// router.get('/ajax',function (req, res){
+//     res.render('./Pages/index', {qoute: "AJAX la so 1"})
+// })
+router.use(bodyParser.json())
 router.get('/', isLoggedIn, (req, res, next) => {
     console.log(req.user);
     res.render('./Pages/index', { user: req.user});
 });
+
+
 
 
 function isLoggedIn(req, res, next) {
@@ -28,6 +34,8 @@ function isLoggedIn(req, res, next) {
 router.get("/about", function (req, res) {
     res.render("./Pages/about");
 })
+// router.use(bodyParser.json())
+
 
 
 
