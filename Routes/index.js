8c311465d81214969f10b1470c492b8d2dc8b9f6
danghieu1407 <danghieu140701@gmail.com>
2021-12-22 18:08:02 +0700

    [fixbug delete]

--- Routes/index.js
+++ Routes/index.js
@@ -89,7 +89,7 @@ router.post('/DeletePost', function (req, res) {
     Post.deleteOne(query, function (err, result) {
         if (err) console.log(err);
         else {
-            res.send(req.body``);
+            res.send(req.body);
         }
     })
 });
